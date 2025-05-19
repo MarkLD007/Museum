522412d38d9fdd0bb8f3c483bd0863be6787130b
MarkLD007 <144927013+MarkLD007@users.noreply.github.com>
2025-05-19 22:07:45 +0800

    更新

---
+++ Museum/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+
+    private float mouseX, mouseY;
+
+    public float mouseSensitivity;
+
+    private float xRotation;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        Player.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
---
+++ Museum/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController cc;
+    public float moveSpeed;
+    public float JumpSpeed;
+    private float horizontalMove, verticalMove;
+    private Vector3 dir;
+    public float gravity;
+    private Vector3 Velocity;
+    public Transform groundCheck;
+    public float CheckRadius;
+    public LayerMask groundLayer;
+    public bool isGround;
+
+    private void Start()
+    {
+       cc= GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, CheckRadius, groundLayer);
+
+        if (isGround && Velocity.y < 0)
+        {
+            Velocity.y = -3f;    
+        }
+
+        horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
+        verticalMove = Input.GetAxis("Vertical") * moveSpeed;
+
+        dir = transform.forward * verticalMove + transform.right * horizontalMove;
+        cc.Move(dir * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGround)
+        {
+            Velocity.y = JumpSpeed;
+        }
+
+        Velocity.y -= gravity * Time.deltaTime;
+        cc.Move(Velocity * Time.deltaTime);
+    }
+}
